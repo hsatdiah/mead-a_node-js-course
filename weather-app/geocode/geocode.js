9a5ca2e881ad6cfa57b1cc4dd660c1b7809cf4a6
hsatdiah <hsatdiah@yandex.com>
2017-03-20 14:43:58 +0300

    Abstracting Callbacks

--- weather-app/geocode/geocode.js
+++ weather-app/geocode/geocode.js
@@ -1,7 +1,6 @@
 const request = require('request');
 
-const geocodeAddress = address => {
-
+const geocodeAddress = (address, callback) => {
   let encodedAddress = encodeURIComponent(address);
 
   request({
@@ -9,14 +8,16 @@ const geocodeAddress = address => {
     json: true,
   }, (error, response, body) => {
     if (error) {
-      console.log('Unable to connect to Google servers...');
+      callback('Unable to connect to Google servers...');
     } else if (body.status === 'ZERO_RESULTS') {
-      console.log('Unable to find specified address.');
+      callback('Unable to find specified address.');
     } else if (body.status === 'OK') {
-      console.log(`Address: ${body.results[0].formatted_address}`);
-      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-    }
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        latitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng,
+      });
+    };
   });
 };
 
