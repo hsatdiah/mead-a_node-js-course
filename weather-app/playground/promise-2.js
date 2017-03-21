75c30784e3005a9504b8105385f6dd461b6ef7fd
hsatdiah <hsatdiah@yandex.com>
2017-03-21 11:27:34 +0300

    applied promises to geocode function

---
+++ weather-app/playground/promise-2.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+const geocodeAddress = address => {
+  return new Promise((resolve, reject) => {
+    let encodedAddress = encodeURIComponent(address);
+    request({
+      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+      json: true,
+    }, (error, response, body) => {
+      if (error) {
+        reject('Unable to connect to Google servers...');
+      } else if (body.status === 'ZERO_RESULTS') {
+        reject('Unable to find specified address.');
+      } else if (body.status === 'OK') {
+        resolve({
+          address: body.results[0].formatted_address,
+          latitude: body.results[0].geometry.location.lat,
+          longitude: body.results[0].geometry.location.lng,
+        });
+      }
+    });
+  });
+};
+
+geocodeAddress('99038').then(location => {
+  console.log(JSON.stringify(location, undefined, 2));
+}, error => {
+  console.log(error);
+});
