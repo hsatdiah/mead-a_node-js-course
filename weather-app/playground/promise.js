520e023235ff1a9312530d66f08b46f274d37ad7
hsatdiah <hsatdiah@yandex.com>
2017-03-21 11:26:52 +0300

    more advanced promises

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -1,3 +1,25 @@
+const asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else {
+        reject('Arguments must be numbers.');
+      }
+    }, 1500);
+  });
+};
+
+asyncAdd(5, 7).then(res => {
+  console.log('Result:', res);
+  return asyncAdd(res, 33);
+}).then((res) => {
+  console.log('Should be 45:', res);
+}).catch(error => {
+  console.log(error);
+});
+
+/*
 const somePromise = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve('Hey. It worked!');
@@ -10,3 +32,4 @@ somePromise.then(message => {
 }, error => {
   console.log('Error: ', error);
 });
+*/
