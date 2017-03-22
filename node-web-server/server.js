eec82ae78f62d916fc4cca0b4f08b0cd608a0bb7
hsatdiah <hsatdiah@yandex.com>
2017-03-22 03:40:37 +0300

    few routes added, sending json

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,7 +3,26 @@ const express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express!');
+  // res.send('<h1>Hello Express!</h1>');
+  res.send({
+    name: 'Miraya',
+    likes: [
+      'anime',
+      'manga',
+      'front-end',
+      'math'
+    ]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About page :3');
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
 });
 
 app.listen(3000);
