0b0ba53d3b6d3fbe761e8b412013dec47c7c591a
hsatdiah <hsatdiah@yandex.com>
2017-03-23 13:36:29 +0300

    helpers and partials

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,6 +3,11 @@ const hbs = require('hbs');
 
 let app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
@@ -10,14 +15,12 @@ app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Homepage',
     welcomeMessage: 'Милости прошу к нашему шалашу',
-    currentYear: new Date().getFullYear(),
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
   });
 });
 
