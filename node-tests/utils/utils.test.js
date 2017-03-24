332367bc954546c93086b3126f5c59ac99a44d03
hsatdiah <hsatdiah@yandex.com>
2017-03-24 21:27:16 +0300

    test square function

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -6,3 +6,10 @@ it('should add two numbers', () => {
     throw new Error(`expected 44, but got ${res}`);
   }
 });
+
+it('should square a number', () => {
+  let res = utils.square(12);
+  if (res !== 144) {
+    throw new Error(`expected 144, but got ${res}`);
+  }
+});
