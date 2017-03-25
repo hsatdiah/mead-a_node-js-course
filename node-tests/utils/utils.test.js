4d76b16bcfe3370ed84286322d44fdcedd53fd20
hsatdiah <hsatdiah@yandex.com>
2017-03-25 16:20:41 +0300

    some sandbox tests to fill expect lib

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,24 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
-  let res = utils.add(33, 11);
-  if (res !== 44) {
-    throw new Error(`expected 44, but got ${res}`);
-  }
+  let res = utils.add(31, 11);
+  expect(res).toBeA('number').toBe(42);
 });
 
 it('should square a number', () => {
   let res = utils.square(12);
-  if (res !== 144) {
-    throw new Error(`expected 144, but got ${res}`);
-  }
+  expect(res).toBeA('number').toBe(144);
+});
+
+it('should expect some values', () => {
+  expect(9).toNotBe(11);
+  expect({name: 'Alex'}).toEqual({name: 'Alex'}); // toBe will fail
+  expect([1,3,5,7]).toInclude(1);
+  expect({
+    name: 'Miraya',
+    age: 25,
+    location: 'unknown',
+  }).toInclude({age: 25}).toExclude({location: 'PL'});
 });
