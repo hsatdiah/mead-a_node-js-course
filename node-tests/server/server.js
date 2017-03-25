cb82aada32292fc7d6b66e50158822f7a66ca3cd
hsatdiah <hsatdiah@yandex.com>
2017-03-25 19:21:57 +0300

    express simple test with supertest and expect

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -3,7 +3,12 @@ const express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Dummy App v1.0'
+  });
 });
 
 app.listen(3000);
+
+module.exports.app = app;
